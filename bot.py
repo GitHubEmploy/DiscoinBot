1784a1e24165794b92b52ed6cf64502d09de91ee
Mohit Varikuti <65371337+GitHubEmploy@users.noreply.github.com>
2020-11-08 20:54:00 -0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+import time
+#---CONFIG---
+
+token = "NTM1OTcyNzA0NDAzMTkzODg4.X6jIMw.XDvpXQ9XY7va7Lq-QjhBe3P_nxk" # https://www.youtube.com/watch?v=tI1lzqzLQCs
+prefix = "!" # This defines the selfbot prefix. You can customize it however you like, but you should probably keep it tiny.
+
+#--- BOT ---
+
+
+
+print("Logging into Discord...")
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+
+    if message.content.startswith('-start'):
+        while True:
+            count = count + 1
+            await message.channel.send('pls beg')
+            time.sleep(26)
+            if count == 139:
+                await message.channel.send('pls hourly')
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(token, bot=False)
