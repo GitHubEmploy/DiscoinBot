cc57b38afdac19a004ebb7f58ec3fd025ce3f3b0
Mohit Varikuti <65371337+GitHubEmploy@users.noreply.github.com>
2020-11-09 12:06:05 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,32 +4,34 @@ import time
 import random
 #---CONFIG---
 
-token = "NTM1OTcyNzA0NDAzMTkzODg4.X6jIMw.XDvpXQ9XY7va7Lq-QjhBe3P_nxk" # https://www.youtube.com/watch?v=tI1lzqzLQCs
+token = "NzcxMTMxNTQ3NjAwNDIwODg0.X6lwIw.wB6nR_LCst43UQxrAAT0-8dbtFE" # https://www.youtube.com/watch?v=tI1lzqzLQCs
 prefix = "!" # This defines the selfbot prefix. You can customize it however you like, but you should probably keep it tiny.
 
 #--- BOT ---
 
-
-
 print("Logging into Discord...")
 
 client = discord.Client()
-
+count = 0
 @client.event
 async def on_message(message):
-
+    global count
     if message.content.startswith('-start'):
         while True:
             count = count + 1
             await message.channel.send('pls beg')
-            time.sleep(int(random.randrange(0, 150, 1)))
-            if count == int(random.randrange(0, 97, 1)):
+            time.sleep(int(random.randrange(0, 5, 1)))
+            await message.channel.send('pls hunt')
+            time.sleep(int(random.randrange(0, 50, 1)))
+            if count >= int(random.randrange(0, 97, 1)):
                 await message.channel.send('pls hourly')
+                count = 0
 @client.event
 async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
+    print(client.guilds)
     print('------')
 
 client.run(token, bot=False)
